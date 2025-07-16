2c37dbe87e4f7eaf864ecb287fa26f82576801a6
KSir <xk_zhao@outlook.com>
2025-07-16 10:34:50 +0800

    路由历史模式改为hash，解决刷新404的问题

--- src/router.js
+++ src/router.js
@@ -39,7 +39,7 @@ const routes = [
 ]
 //路由对象
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes //上面的路由数组
 })
 
